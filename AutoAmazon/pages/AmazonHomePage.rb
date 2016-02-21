69f23f19b963b03eff663bd1c26d061283efe85b
janeyzhou <771693395@qq.com>
2016-02-21 11:00:01 +0800

    2

---
+++ AutoAmazon/pages/AmazonHomePage.rb
@@ -0,0 +1,17 @@
+module Amazon
+  class AmazonHomePage < Page
+
+
+    def layer
+      @browser
+    end
+    def search_bar
+      layer.text_field(id: "twotabsearchtextbox")
+    end
+    def search_button
+      layer.div(:class => 'nav-right')
+    end
+  end
+  Pages.register_page(Amazon::AmazonHomePage, :amazon_home_page)
+end
+
